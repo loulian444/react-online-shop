1a14bfa56cc392a328acc0287362bd379dddedee
Loulian Liu <3l.liu444@gmail.com>
2023-08-03 16:11:28 -0400

    not sure what even happened for the past 20 min

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -10,7 +10,14 @@ const Shop = ({ productsList }) => {
           console.log(product);
           return (
             <Grid item xs={3} key={product.id}>
-              <p>{product.title}</p>
+              <Grid container spacing={1} className="itemHeader">
+                <Grid item xs={9}>
+                  <p>{product.title}</p>
+                </Grid>
+                <Grid item xs={3}>
+                  <button>Details</button>
+                </Grid>
+              </Grid>
               <img className="thumbnail" src={product.thumbnail} />
             </Grid>
           );
